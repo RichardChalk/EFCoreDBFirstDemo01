5b6090251b333ce4d253816254d741e1bc6e25e6
Richard Chalk <barmybrit@gmail.com>
2022-11-21 12:36:46 +0100

    #3 Added DbContext

---
+++ EFCoreDBFirstDemo01/Models/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreDBFirstDemo01.Models
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=EFCoreCodeFirstOrderDemo;Trusted_Connection=True;TrustServerCertificate=true");
+        }
+    }
+}
