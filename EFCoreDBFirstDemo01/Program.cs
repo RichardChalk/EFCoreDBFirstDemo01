0f16711ab93af16a7a520d9d165b19afd94aa635
Richard Chalk <barmybrit@gmail.com>
2022-11-21 12:44:03 +0100

    #4 Installed last nuget package Microsoft.EntityFrameworkCore.Design

--- EFCoreDBFirstDemo01/Program.cs
+++ EFCoreDBFirstDemo01/Program.cs
@@ -1,4 +1,13 @@
 ﻿// Step 1
-// Install Microsoft.EntityFrameworkCore.SqlServer
-// Install Microsoft.EntityFrameworkCore.Tools
+// Installera Microsoft.EntityFrameworkCore.SqlServer
+// Installera Microsoft.EntityFrameworkCore.Tools
+
+// Step 2
+// Skapa alla klasser som behövs för din databas
+
+// Step 3
+// Skapa din DbContext med relevanta Dbsets
+
+// Step 4
+// Installera ett sista nuget paket - Microsoft.EntityFrameworkCore.Design
 
